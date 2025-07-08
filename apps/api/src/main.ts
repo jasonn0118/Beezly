36f7d734eccdbddb7d7d595de1843c5502a342a8
Samuel Kim <ghassad87@gmail.com>
2025-07-08 18:12:52 -0400

    chore(api): add INestApplication type for app bootstrap and re-verify dependencies

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { INestApplication } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app: INestApplication = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
     .setTitle('Beezly API')
