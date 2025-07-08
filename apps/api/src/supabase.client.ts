359544f4b30ec47b486777a00d5449be405873f5
Samuel Kim <ghassad87@gmail.com>
2025-07-08 17:06:49 -0400

    Complete merge from staging after resolving pnpm-lock.yaml conflict

--- apps/api/src/supabase.client.ts
+++ apps/api/src/supabase.client.ts
@@ -5,5 +5,4 @@ dotenv.config();
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-console.log('URL', supabaseUrl);
 export const supabase = createClient(supabaseUrl, supabaseKey);
