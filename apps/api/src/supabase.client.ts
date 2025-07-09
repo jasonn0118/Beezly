20f9aa5bb0978683901a632aa2042dfc36392c5c
Samuel Kim <ghassad87@gmail.com>
2025-07-08 21:31:53 -0400

    fix(api): ensure Supabase client always initialized to avoid undefined errors

--- apps/api/src/supabase.client.ts
+++ apps/api/src/supabase.client.ts
@@ -1,19 +1,7 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
-let supabase: SupabaseClient | undefined;
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL || 'http://localhost:5000';
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'dummy_key';
 
-if (
-  process.env.NEXT_PUBLIC_SUPABASE_URL &&
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-) {
-  supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  );
-} else {
-  console.warn(
-    'Supabase environment variables not set. Supabase client will not be initialized.',
-  );
-}
-
-export { supabase };
+export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
