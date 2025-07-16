6a83ccdbcefc3954f0be0bd4d5eab5862fb60521
Samuel Kim <ghassad87@gmail.com>
2025-07-16 15:32:49 -0400

    Edited app.e2e-spec file

--- apps/api/test/app.e2e-spec.ts
+++ apps/api/test/app.e2e-spec.ts
@@ -78,6 +78,6 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('API is running!');
+      .expect('Hey Guys! Beezly API is running!');
   });
 });
