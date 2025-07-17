1ff065a71c56cb9cc81167853979d453bc10795f
claire-pako <clairepark031@gmail.com>
2025-07-17 11:44:11 -0700

    Modify Navigator #67

---
+++ apps/mobile/app/(tabs)/rank.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import RankCard from '../../components/RankCard';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <RankCard />
+    </View>
+  );
+}
