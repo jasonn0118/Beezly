6c17d061669e600127421b7d677ef47a31d65e8b
claire-pako <clairepark031@gmail.com>
2025-07-29 17:01:02 -0700

    Create Common Page(Navigation)

---
+++ apps/mobile/app/(tabs)/index/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import Search from '../../../src/components/search/search';
+
+export default function SettingsScreen() {
+  return (
+    <View style={{ flex: 1}}>
+      <Search />
+    </View>
+  );
+}
